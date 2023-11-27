b2053dc4eafbec3cb45776c1b5d2ddf0b486051f
yan-s <110732997+Toppics@users.noreply.github.com>
2023-11-27 23:11:17 +0100

    Update intersection3D.py

--- intersection3D.py
+++ intersection3D.py
@@ -5,7 +5,7 @@ Created on Mon Nov 27 22:00:18 2023
 @author: yan-s
 """
 
-def intersection(A, B, C, D):    
+def intersection3D(A, B, C, D):    
     """Tel que [AB] et [CD] des segments"""
     u=((C[0]-A[0])*(C[1]-D[1])-(C[1]-A[1])*(C[0]-D[0]))/((C[1]-D[1])*(B[0]-A[0])-(C[0]-D[0])*(B[1]-A[1]))
     v=((B[1]-A[1])*(C[0]-A[0])-(B[0]-A[0])*(C[1]-A[1]))/((B[1]-A[1])*(C[0]-D[0])-(B[0]-A[0])*(C[1]-D[1]))
